fef66a08291ea55ec3526b2ab0f0da6e5690b0c7
Lancern <xh19980325@126.com>
2018-08-29 17:48:13 +0800

    Partition interface

---
+++ miniFS.Core/include/fs/MFSFSEntry.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+/*
+
+class MFSFSEntry
+表示文件系统树的一个节点。
+
+*/
+
+class MFSFSEntry
+{
+
+};
--- miniFS.Core/src/fs/MFSPartition.cpp
+++ miniFS.Core/src/fs/MFSPartition.cpp
@@ -1,4 +1,55 @@
 #include "../../include/fs/MFSPartition.h"
 
+MFSPartition::MFSPartition(MFSBlockDevice * device)
+    : _device(device)
+{
+}
 
+MFSPartition::~MFSPartition()
+{
+    Close();
+}
 
+MFSBlockDevice * MFSPartition::GetDevice() const
+{
+    return _device;
+}
+
+bool MFSPartition::IsRaw() const
+{
+    // TODO: Implement MFSPartition::IsRaw().
+    return false;
+}
+
+UINT64 MFSPartition::GetTotalSpaceInBytes() const
+{
+    // TODO: Implement MFSPartition::GetTotalSpaceInBytes().
+    return UINT64();
+}
+
+UINT64 MFSPartition::GetFreeSpaceInBytes() const
+{
+    // TODO: Implement MFSPartition::GetFreeSpaceInBytes().
+    return UINT64();
+}
+
+void MFSPartition::BuildFileSystem()
+{
+    // TODO: Implement MFSPartition::BuildFileSystem().
+}
+
+MFSFSEntry * MFSPartition::GetRoot() const
+{
+    // TODO: Implement MFSPartition::GetRoot().
+    return nullptr;
+}
+
+void MFSPartition::Flush()
+{
+    // TODO: Implement MFSPartition::Flush().
+}
+
+void MFSPartition::Close()
+{
+    // TODO: Implement MFSPartition::Close().
+}
