d1d49e45eb60733feb655061a25286486c98b64c
peterzpy <531637170@qq.com>
2018-09-08 18:40:23 +0800

    temp

--- miniFS/main.cpp
+++ miniFS/main.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 #include "../miniFS.Core/include/MFSString.h"
 #include "include/MFSCommand.h"
+#include "../miniFS.Core/include/stream/MFSStream.h"
+#include "../miniFS.Core/include/stream/MFSStreamWriter.h"
+#include "../miniFS.Core/include/stream/MFSStreamReader.h"
 #include "include/MFSTest.h"
 #include "include/io/MFSConsole.h"
 
@@ -10,33 +13,33 @@ int main()
 {
 	MFSTest command;
 	MFSConsole *point = MFSGetDefaultConsole();
+	MFSStreamWriter writer();
+	//while (1)
+	//{
+	//	std::vector<WCHAR> split = {L'|'};
+	//	std::vector<MFSString> subString;
+	//	point->Log(L"miniFS>");
+	//	MFSString strInput = point->ReadLine();
+	//	/*point->SetForegroundColor(MFSConsoleColors::Red);
+	//	point->SetBackgroundColor(MFSConsoleColors::Green);*/
+	//	subString = strInput.SplitName(split);
+	//	for (const MFSString & part : subString) {
 
-	while (1)
-	{
-		MFSTestunit *tmp = command.Chead->link;
-		std::vector<WCHAR> split = {L'|'};
-		std::vector<MFSString> subString;
-		point->Log(L"miniFS>");
-		MFSString strInput = point->ReadLine();
-		/*point->SetForegroundColor(MFSConsoleColors::Red);
-		point->SetBackgroundColor(MFSConsoleColors::Green);*/
-		subString = strInput.SplitName(split);
-		for (const MFSString & part : subString) {
-
-			std::vector<WCHAR> splitNew = { L' ', L'\t'};
-			std::vector<MFSString> paragrameter;
-			paragrameter = part.SplitName(splitNew);
-			/*for (int i = 0; i < 14; i++)
-			{
-				if (tmp->base->Accept(paragrameter[0]))
-				{
-					paragrameter.erase(paragrameter.begin());
-					tmp->base->Action(paragrameter);
-					break;
-				}
-				tmp = tmp->link;
-			}*/
-		}
-	}
+	//		MFSTestunit *tmp = command.Chead->link;
+	//		std::vector<WCHAR> splitNew = { L' ', L'\t'};
+	//		std::vector<MFSString> paragrameter;
+	//		paragrameter = part.SplitName(splitNew);
+	//		for (int i = 0; i < 14; i++)
+	//		{
+	//			if (tmp->base->Accept(paragrameter[0]))
+	//			{
+	//				paragrameter.erase(paragrameter.begin());
+	//				tmp->base->Action(paragrameter);
+	//				break;
+	//			}
+	//			tmp = tmp->link;
+	//		}
+	//	}
+	//}
 	return 0;
 }
