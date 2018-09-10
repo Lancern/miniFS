a828f235274030a829f6e67254890b65a4e703f8
houdewei <houdeweihdw@foxmail.com>
2018-09-10 14:38:07 +0800

    new commands

--- miniFS/src/command/DirCommand.cpp
+++ miniFS/src/command/DirCommand.cpp
@@ -9,7 +9,17 @@ bool DirCommand::Accept(const MFSString & string) const
 
 void DirCommand::Action(const std::vector<MFSString> & argv) const
 {
-
+	if (argv.size() != 0)
+	{
+		printf("command is wrong\n");
+		return;
+	}
+	MFSDataSpace * space = MFSDataSpace::GetActiveDataSpace();
+	std::vector<MFSString> fileList = space->GetDirectories(space->GetWorkingDirectory());
+	for (MFSString file : fileList)
+	{
+		std::wcout << file.GetRawString() << std::endl;
+	}
 }
 
 DirCommand::DirCommand()
--- miniFS/src/command/MountCommand.cpp
+++ miniFS/src/command/MountCommand.cpp
@@ -26,8 +26,6 @@ void MountCommand::Action(const std::vector<MFSString> & argv) const
 		space = new MFSDataSpace(argv[0]);
 		MFSDataSpace::SetActiveDataSpace(space);
 		space->Format();
-		std::wcout << L"TotalSpaceInBytes " <<space->GetTotalSpaceInBytes() << std::endl;
-		std::wcout << L"FreeSpaceInBytes " <<space->GetFreeSpaceInBytes() << std::endl;
 	}
 	catch (MFSException & ex)
 	{
