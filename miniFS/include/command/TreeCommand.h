9ebbfb59031445a7092f9da4319816db1e6d1146
peterzpy <531637170@qq.com>
2018-09-11 11:54:12 +0800

    tree save

--- miniFS/include/command/TreeCommand.h
+++ miniFS/include/command/TreeCommand.h
@@ -9,3 +9,5 @@ public:
 
 	TreeCommand();
 };
+
+void tree(int depth, std::vector<bool> & flag);
--- miniFS/src/command/TreeCommand.cpp
+++ miniFS/src/command/TreeCommand.cpp
@@ -21,6 +21,64 @@ void TreeCommand::Action(const std::vector<MFSString> & argv) const
 		point->Log(L"÷∏¡Ó ‰»Î”–ŒÛ\n");
 		return;
 	}
+	try
+	{
+		std::vector<bool>flag;
+		tree(0, flag);
+	}
+	catch (MFSException & ex)
+	{
+		point->LogLine(ex.GetExceptMessage());
+	}
+}
+
+void tree(int depth, std::vector<bool> & flag)
+{
+	MFSDataSpace *space = MFSDataSpace::GetActiveDataSpace();
+	MFSConsole *point = MFSConsole::GetDefaultConsole();
+	MFSString path = space->GetWorkingDirectory();
+	std::vector<MFSString>subdir = space->GetDirectories(path);
+	while (flag.size() != depth)
+		flag.pop_back();
+	if (depth == 0)
+	{
+		for (int i = 0; i < depth; i++)
+			point->Log(L"    ");
+		point->LogLine(path);
+		flag.push_back(1);
+	}
+	if (subdir.size() == 0) flag.empty();
+	for (auto subpath : subdir)
+	{
+		space->SetWorkingDirectory(subpath);
+		if (subpath == subdir.back())
+		{
+			for (int i = 0; i < depth; i++)
+			{
+				if (flag[i] == 1)
+					point->Log(L"©¶   ");
+				else
+					point->Log(L"    ");
+			}
+			point->Log(L"©∏©§©§ ");
+			flag.push_back(0);
+		}
+		else
+		{
+			for (int i = 0; i < depth; i++)
+			{
+				if (flag[i] == 1)
+					point->Log(L"©¶   ");
+				else
+					point->Log(L"    ");
+			}
+			point->Log(L"©¿©§©§ ");
+			flag.push_back(1);
+		}
+		point->LogLine(subpath);
+		tree(depth + 1, flag);
+		space->SetWorkingDirectory(path);
+	}
 }
 
 TreeCommand::TreeCommand()
